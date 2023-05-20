2bb36136978d901d258981c7b9ea8b909cbdc0eb
Jessvic <128914001+Jessvic@users.noreply.github.com>
2023-05-20 11:32:10 -0600

    Add files via upload

---
+++ SalesReason/Services/SalesReasonService.cs
@@ -0,0 +1,41 @@
+﻿using SalesReason.Data;
+using System.Collections.Generic;
+
+
+namespace SalesReason.Services
+{
+    public class SalesReasonService
+    {
+        private readonly SalesReasonRepository salesReasonRepository;
+
+        public SalesReasonService(SalesReasonRepository salesReasonRepository)
+        {
+            this.salesReasonRepository = salesReasonRepository;
+        }
+
+        public IEnumerable<SalesReason> GetAllSalesReasons()
+        {
+            return salesReasonRepository.GetAllSalesReasons();
+        }
+
+        public SalesReason GetSalesReasonByID(int salesReasonID)
+        {
+            return salesReasonRepository.GetSalesReasonByID(salesReasonID);
+        }
+
+        public void AddSalesReason(SalesReason salesReason)
+        {
+            salesReasonRepository.AddSalesReason(salesReason);
+        }
+
+        public void UpdateSalesReason(SalesReason salesReason)
+        {
+            salesReasonRepository.UpdateSalesReason(salesReason);
+        }
+
+        public void DeleteSalesReason(int salesReasonID)
+        {
+            salesReasonRepository.DeleteSalesReason(salesReasonID);
+        }
+    }
+}
